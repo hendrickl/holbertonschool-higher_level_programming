e80eb848dc040ba31334c17b2582a825ee16ac51
hendrickl <h.lincertin@gmail.com>
2023-11-23 06:23:17 -0800

    Task 1: fix

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -10,6 +10,7 @@ elif lastDigit == 0:
     print(str0, number, "is", lastDigit, "and is 0")
 elif lastDigit < 6 and lastDigit != 0:
     if number < 0:
-        lastDigit = number % 10
+        global lastDigit
+        lastDigit *= -1
         print(str0, number, "is", lastDigit, "and is less than 6 and not 0")
     print(str0, number, "is", lastDigit, "and is less than 6 and not 0")
