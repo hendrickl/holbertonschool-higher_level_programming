aa6dfa83f4fafb11e33377fe40a94715b95b10e1
hendrickl <h.lincertin@gmail.com>
2023-11-26 11:08:18 -0800

    Task 6: fix break statement

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,5 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print('{:02}'.format(i * 10 + j))
+            break
         print('{:02}'.format(i * 10 + j), end=", ")
