29c451c4939991b79269e17e758a745c5a3a4f45
hendrickl <h.lincertin@gmail.com>
2023-11-28 09:38:29 -0800

    Task 0: fix format for docstrings

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -2,6 +2,9 @@
 """Creates an instance of Square and prints its type and dict"""
 Square = __import__('0-square').Square
 
+"""Create an instance"""
 my_square = Square()
+
+"""Print the type and dictinary of the instance"""
 print(type(my_square))
 print(my_square.__dict__)
