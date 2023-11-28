e215946e479f2854e7e3afd165d6bcb121d2b609
hendrickl <h.lincertin@gmail.com>
2023-11-28 09:53:45 -0800

    Task 0: add docstring for module

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Empty class Square that defines a square"""
+"""Declaration of class Square"""
 
 
 class Square:
+    """Empty class Square that defines a square"""
     pass
