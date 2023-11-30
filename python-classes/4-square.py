d04255c8cb7b9ccf9ed848d09dd373b70d851d64
hendrickl <h.lincertin@gmail.com>
2023-11-30 13:12:53 -0800

    Task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Declaration of class Square"""
+
+
+class Square:
+    """Class Square with size"""
+    def __init__(self, size=0):
+        """Initialization method with size"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
