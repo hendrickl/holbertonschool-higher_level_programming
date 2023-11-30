a4019a55ff64bb3fa47aeebc7220e9403d8273d1
a3940d934f5c <8174@holbertonstudents.com>
2023-11-30 22:49:24 +0000

    Task 1: fix data output

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -4,12 +4,11 @@ const fs = require('fs');
 
 // Get file path and content from the command line arguments
 const filePath = process.argv[2];
+const content = process.argv[3];
 
 // Write the content to the file in utf-8
-fs.writeFile(filePath, 'C is fun', 'utf-8', (err) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(`${filePath}`);
   }
 });
