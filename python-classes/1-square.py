14a86aaa98051f90496d3ad1b9b4930c7b4adfb8
hendrickl <h.lincertin@gmail.com>
2023-11-30 11:52:21 -0800

    Task 1: add docstring

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,6 +2,7 @@
 """Declaration of class Square"""
 
 class Square:
+    """Class Square with size"""
     def __init__(self, size):
         """Initialization method with size"""
         self.__size = size
