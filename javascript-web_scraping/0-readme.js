21ed416d651518744f0741225ed137e127e53fa2
a3940d934f5c <8174@holbertonstudents.com>
2023-11-30 22:07:14 +0000

    Task 0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// Get the file path from the command line argument
+const filePath = process.argv[2];
+
+// Read the content of the file in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
