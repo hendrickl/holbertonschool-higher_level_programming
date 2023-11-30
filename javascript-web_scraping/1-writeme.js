0967e753cb9c74088a4af06833e1b7e9a09091a6
a3940d934f5c <8174@holbertonstudents.com>
2023-11-30 22:35:19 +0000

    Task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+// Get file path and content from the command line arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write the content to the file in utf-8
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Content has been written to ${filePath}`);
+  }
+});
