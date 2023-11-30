31905c3bfb94c74d021af0523f317f31f6a8b6fa
hendrickl <h.lincertin@gmail.com>
2023-11-30 11:56:13 -0800

    Task 1: add docstring

--- python-classes/1-main.py
+++ python-classes/1-main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""Creates an instance of Square and prints its type and dict"""
 Square = __import__('1-square').Square
 
+"""Create an instance"""
 my_square = Square(3)
+
+"""Print some informations"""
 print(type(my_square))
 print(my_square.__dict__)
 
