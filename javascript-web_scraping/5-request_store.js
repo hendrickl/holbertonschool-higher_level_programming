11ccb878f62bce1bf52b027ed46b9d0844190766
a3940d934f5c <8174@holbertonstudents.com>
2023-12-01 08:46:47 +0000

    Task 5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+// Get URL and file path from the command line arguments
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Make a GET request to the specified URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    // Write the body response to the specified file in utf-8
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+      if (writeError) {
+        console.error(writeError);
+      } else {
+        console.log(`${filePath}`);
+      }
+    });
+  }
+});
